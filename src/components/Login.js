0ea1ae9a66f1cfa608ba2551573998f4b32107e0
Axel Darren Suryanto <axeldarren.suryanto@gmail.com>
2024-10-13 17:54:36 +0700

    delete github button

--- src/components/Login.js
+++ src/components/Login.js
@@ -129,12 +129,6 @@ export default function Login() {
               Sign in with Google
             </button>
             <button
-              onClick={() => handleOAuthLogin('github')}
-              className="flex w-full justify-center rounded-md bg-gray-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
-            >
-              Sign in with GitHub
-            </button>
-            <button
               onClick={() => handleOAuthLogin('discord')}
               className="flex w-full justify-center rounded-md bg-blue-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
             >
